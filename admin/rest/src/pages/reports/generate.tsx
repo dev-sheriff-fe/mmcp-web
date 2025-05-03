d5c168f836140bb43316c269ded9a71d5d77afb1
Mayowa Sodipo <sodipomayowa7@gmail.com>
2025-05-03 18:05:28 +0100

    fix:generate report

--- admin/rest/src/pages/reports/generate.tsx
+++ admin/rest/src/pages/reports/generate.tsx
@@ -1,8 +1,11 @@
 import { useTranslation } from 'next-i18next';
 import { Table } from '@/components/ui/table';
 import useGenerateReport from '@/hooks/useGenerateReport';
+import Layout from '@/components/layouts/admin';
+
 import { generateReportColumn } from '@/utils/locals';
-const generate = () => {
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+const Generate = () => {
   const { t } = useTranslation();
   const { reports, isLoading, code, downloadMutation } = useGenerateReport();
   return (
@@ -17,4 +20,10 @@ const generate = () => {
   );
 };
 
-export default generate;
+export default Generate;
+Generate.Layout = Layout;
+export const getStaticProps = async ({ locale }: any) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['table', 'form', 'common'])),
+  },
+});
