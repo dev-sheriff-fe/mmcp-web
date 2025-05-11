2dd2fa21de6f461590453e600e1476c08f31c26e
Mayowa Sodipo <sodipomayowa7@gmail.com>
2025-05-11 19:16:02 +0100

    fix:transaction details

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import Loader from '@/components/ui/loader/loader';
-import { useLogoutMutation } from '@/data/user';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { logout } from '@/utils/auth-utils';
 
 function SignOut() {
   const { t } = useTranslation();
-  const { mutate: logout } = useLogoutMutation();
+  // const { mutate: logout } = useLogoutMutation();
 
   useEffect(() => {
     logout();
